b668985fc523e04e4c7ad8400be19e8f73aad7a1
unknown <tu7gbzmx6@code.edu.az>
2022-03-29 10:13:28 +0400

    ConsoleApp59

---
+++ ConsoleApp59/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp59
+{
+    internal class Program
+    {
+        static void Main(string[] args, string groupNo)
+        {
+            string fullname;
+            do
+            {
+                Console.WriteLine("Student FullName daxil edin:");
+                fullname = Console.ReadLine();
+
+            } while (!Student.CheckFullname(fullname));
+
+            string groupNo;
+            do
+            {
+                Console.WriteLine("Qrup Nomresini daxil edin:");
+                groupNo = Console.ReadLine();
+            } while (Student.CheckGroupNo(groupNo));
+
+            Console.WriteLine("Studen Yasini daxil edin:");
+            int Age=Convert.ToInt32(Console.ReadLine());
+
+            Student student=new Student(fullname,groupNo,Age);
+        }
+            
+       
+    }
+}
---
+++ ConsoleApp59/Student.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp59
+{
+    internal class Student
+    {
+        public Student(string fullname,string groupNo,int age)
+        {
+            this.GroupNo = groupNo;
+            this.Fullname = fullname;
+
+        }
+        private string _fullname;
+        private string _groupNo;
+        
+        public string GroupNo
+        {
+            get => this._groupNo;
+            set
+            {
+                if (CheckGroupNo(value))
+                    this._groupNo = value;
+            }
+        }
+        public string Fullname
+        {
+            get=>this._fullname;
+            set
+            {
+                if(CheckFullname(value))
+                    this.Fullname = value;
+            }
+        }
+        public static bool CheckGroupNo(this string GroupNo)
+        {
+            for (int i = 0; i < GroupNo.Length; i++)
+            {
+                if (char.IsUpper(GroupNo[0]) && GroupNo.Length > 2 && GroupNo.Length < 4)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public static bool CheckFullname(this string Fullname)
+        {
+            string[] sentence = Fullname.Split(' ');
+            for (int i = 0; i < Fullname.Length; i++)
+            {
+                if (sentence.Length == 2)
+                {
+                    foreach (var item in sentence)
+                    {
+                        if (char.IsUpper(sentence[i]))
+                        {
+                            return false;
+                        }
+                    }
+                    return true;
+                }
+            }
+            return false;
+            
+        }
+
+    }
+}
